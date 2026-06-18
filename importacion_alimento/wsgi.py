3412f8ca1a4728b8c5bb2be71712d7963b69bb0e
34miguel63 <bmiguelito488@gmail.com>
2026-06-18 09:24:06 -0400

    Primer commit - Proyecto Importación de Alimentos

---
+++ importacion_alimento/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for importacion_alimento project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'importacion_alimento.settings')
+
+application = get_wsgi_application()
